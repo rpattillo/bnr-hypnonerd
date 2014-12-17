3b8f2249fd2c65536e29edc1416d42e5836baff3
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-16 20:49:13 -0500

    Initial Commit

---
+++ HypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by Ricky Pattillo on 12/16/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
