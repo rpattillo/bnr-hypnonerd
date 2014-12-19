020106c60dabf16580834148b3dd23371540af77
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-18 21:57:46 -0500

    Add hypnotic message
    Demonstrates UIResponder
    Takes a text field and adds the message 20 times to the view, with a
    parallax effect.

--- HypnoNerd/HypnosisViewController.m
+++ HypnoNerd/HypnosisViewController.m
@@ -9,6 +9,9 @@
 #import "HypnosisViewController.h"
 #import "HypnosisView.h"
 
+@interface HypnosisViewController() <UITextFieldDelegate>
+@end
+
 @implementation HypnosisViewController
 
 #pragma mark - Initializers
@@ -27,11 +30,67 @@
 }
 
 
+#pragma mark -
+
+- (void)drawHypnoticMessage:(NSString *)message
+{
+   for (int i = 0; i < 20; i++) {
+      UILabel *messageLabel = [[UILabel alloc] init];
+      
+      messageLabel.backgroundColor = [UIColor clearColor];
+      messageLabel.textColor = [UIColor whiteColor];
+      messageLabel.text = message;
+      
+      [messageLabel sizeToFit];
+      
+      int width = (int)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+      int x = arc4random() % width;
+      
+      int height = (int)(self.view.bounds.size.height - messageLabel.bounds.size.height);
+      int y = arc4random() % height;
+      
+      CGRect frame = messageLabel.frame;
+      frame.origin = CGPointMake(x, y);
+      messageLabel.frame = frame;
+      
+      [self.view addSubview:messageLabel];
+      
+      UIInterpolatingMotionEffect *motionEffect;
+      motionEffect = [[UIInterpolatingMotionEffect alloc]
+                      initWithKeyPath:@"center.x"
+                      type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+      motionEffect.minimumRelativeValue = @(-25);
+      motionEffect.maximumRelativeValue = @(25);
+      [messageLabel addMotionEffect:motionEffect];
+      
+      motionEffect = [[UIInterpolatingMotionEffect alloc]
+                      initWithKeyPath:@"center.y"
+                      type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+      motionEffect.minimumRelativeValue = @(-25);
+      motionEffect.maximumRelativeValue = @(25);
+      [messageLabel addMotionEffect:motionEffect];
+      
+   }
+}
+
+
 #pragma mark - Overrides
 
 - (void)loadView
 {
    HypnosisView *backgroundView = [[HypnosisView alloc] init];
+   
+   CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+   UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+   
+   textField.borderStyle = UITextBorderStyleRoundedRect;
+   textField.placeholder = @"Hypnotize me";
+   textField.returnKeyType = UIReturnKeyDone;
+   
+   textField.delegate = self;
+   
+   [backgroundView addSubview:textField];
+   
    self.view = backgroundView;
 }
 
@@ -41,6 +100,18 @@
    [super viewDidLoad];
    
    NSLog(@"HypnosisViewController loaded its view.");
+}
+
+
+#pragma mark - UITextField delegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+   [self drawHypnoticMessage:textField.text];
+   
+   textField.text = @"";
+   [textField resignFirstResponder];
+   return YES;
 }
 
 @end
